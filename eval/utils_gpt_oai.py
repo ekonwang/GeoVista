992d5a1cb393910dda7ab1942f1986163250c109
rickykwang <rickykwang@tencent.com>
2025-12-01 17:24:55 +0800

    update geovista-bench inference and evaluation pipeline

--- eval/utils_gpt_oai.py
+++ eval/utils_gpt_oai.py
@@ -20,7 +20,7 @@ from utils import print_hl, print_error
 
 
 client = OpenAI(
-    api_key=,
+    api_key=os.environ.get("OPENAI_API_KEY", ""),
     # base_url="https://172.203.11.191:3826/v1",
 )
 MODEL_NAME = 'gpt-5'
